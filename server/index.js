faea655f1bec736043eb04d3e2705d191372d93a
AnggaWikaNugraha <anggawika18@gmail.com>
2021-11-19 00:19:54 +0700

    get posts

--- server/index.js
+++ server/index.js
@@ -3,14 +3,31 @@ const gql = require('graphql-tag');
 const mongoose = require('mongoose')
 const { MONGGO_DB } = require('./config')
 
+const Post = require('./models/Post');
+const User = require('./models/User');
+
 const typeDefs = gql`
-    type Query {
-        sayHi : String!
+    type Post{
+        id : ID!
+        body : String!
+        createdAt : String!
+        username : String!
     }
-`
+    type Query {
+        getPosts : [Post]
+    }
+`;
+
 const resolvers = {
     Query: {
-        sayHi: () => 'hello World'
+        async getPosts() {
+            try {
+                const posts = await Post.find()
+                return posts
+            } catch (error) {
+                throw new Error(error)
+            }
+        }
     }
 }
 
