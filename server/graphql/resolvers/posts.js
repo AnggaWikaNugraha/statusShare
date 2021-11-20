db42909347963317cb9ae83c571de62320b746d6
AnggaWikaNugraha <anggawika18@gmail.com>
2021-11-20 22:27:59 +0700

    get detail post

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -9,6 +9,18 @@ module.exports = {
             } catch (error) {
                 throw new Error(error)
             }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+                if (post) {
+                    return post;
+                } else {
+                    throw new Error('Post not found');
+                }
+            } catch (err) {
+                throw new Error(err);
+            }
         }
     }
 }
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -24,6 +24,7 @@ module.exports = gql`
 
     type Query {
         getPosts : [Post]
+        getPost(postId: ID!): Post
     }
     type Mutation {
         register(registerInput: RegisterInput): User!
