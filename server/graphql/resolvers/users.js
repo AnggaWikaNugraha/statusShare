62b71d0991df7f0eda8d6ab5b785dc848dd4cb4a
AnggaWikaNugraha <anggawika18@gmail.com>
2021-11-20 21:19:16 +0700

    login

--- server/graphql/resolvers/users.js
+++ server/graphql/resolvers/users.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const { validateRegisterInput } = require('../../util/validation')
+const { validateRegisterInput, validateLoginInput } = require('../../util/validation')
 const { UserInputError } = require('apollo-server');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
@@ -19,6 +19,34 @@ function generateToken(user) {
 
 module.exports = {
     Mutation: {
+        async login(_, { username, password }) {
+            const { errors, valid } = validateLoginInput(username, password);
+
+            if (!valid) {
+                throw new UserInputError('Errors', { errors });
+            }
+
+            const user = await User.findOne({ username });
+
+            if (!user) {
+                errors.general = 'User not found';
+                throw new UserInputError('User not found', { errors });
+            }
+
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                errors.general = 'Wrong crendetials';
+                throw new UserInputError('Wrong crendetials', { errors });
+            }
+
+            const token = generateToken(user);
+
+            return {
+                ...user._doc,
+                id: user._id,
+                token
+            };
+        },
         async register(_, { registerInput: { username, email, password, confirmPassword } }) {
             // Validate user data
             const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
