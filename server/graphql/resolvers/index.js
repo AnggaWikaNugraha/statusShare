383c64fcf8ac8a3dc144bec0486fdb4e539765a4
AnggaWikaNugraha <anggawika18@gmail.com>
2021-11-21 12:12:01 +0700

    count likes and coments

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -3,6 +3,11 @@ const userResolver = require('./users')
 const commentsResolvers = require('./comments')
 
 module.exports = {
+    // panggil type post
+    Post: {
+        likeCount: (parent) => parent.likes.length,
+        commentCount: (parent) => parent.comments.length
+    },
     Query: {
         ...postResolver.Query,
     },
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -19,6 +19,8 @@ module.exports = gql`
         username : String!
         comments: [Comment]!
         likes: [Like]!
+        likeCount: Int!
+        commentCount: Int!
     }
     type User {
         id: ID!
