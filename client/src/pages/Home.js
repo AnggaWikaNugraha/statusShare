8d2e0bde906bd0f013e3b5e043a7277ed644b9ee
AnggaWikaNugraha <anggawika18@gmail.com>
2021-12-11 17:51:42 +0700

    post status

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,31 +2,43 @@ import { useQuery } from '@apollo/client';
 import React, { useContext } from 'react';
 import { Grid, Transition } from 'semantic-ui-react';
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
+import { AuthContext } from '../context/auth';
 import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
+    const { user } = useContext(AuthContext);
 
     const { loading, data } = useQuery(FETCH_POSTS_QUERY)
     return (
-        <Grid columns={3}>
-            <Grid.Row className="page-title">
-                <h1>Recent Posts</h1>
-            </Grid.Row>
-            <Grid.Row>
-                {loading ? (
-                    <h1>Loading posts..</h1>
-                ) : (
-                    <Transition.Group>
-                        {data?.getPosts &&
-                            data?.getPosts.map((post) => (
-                                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                                    <PostCard post={post} />
-                                </Grid.Column>
-                            ))}
-                    </Transition.Group>
-                )}
-            </Grid.Row>
-        </Grid>
+        <>
+
+            <Grid columns={3}>
+                <Grid.Row className="page-title">
+                    <h1>Recent Posts</h1>
+                </Grid.Row>
+
+                <Grid.Row>
+                    {user && (
+                        <Grid.Column>
+                            <PostForm />
+                        </Grid.Column>
+                    )}
+                    {loading ? (
+                        <h1>Loading posts..</h1>
+                    ) : (
+                        <Transition.Group>
+                            {data?.getPosts &&
+                                data?.getPosts.map((post) => (
+                                    <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                                        <PostCard post={post} />
+                                    </Grid.Column>
+                                ))}
+                        </Transition.Group>
+                    )}
+                </Grid.Row>
+            </Grid>
+        </>
     );
 }
 
